c2aff4796e5c30eaa2e42756846c96d7d4b2e600
niyaz <niyaz.hashmi@rediffmail.com>
2024-02-27 10:46:34 +0530

    deleted db test code

--- tests/testdbconnection.spec.js
+++
@@ -1,8 +0,0 @@
-import { test } from "@playwright/test";
-import {MysqlAwsRdsConnection} from "../utils/mysqldbconnect";
-
-test.skip("Test MYSQL Database Connection with AWS RDS", async({})=>{
-
-const dbObj = new MysqlAwsRdsConnection();
-dbObj.dbConnect();
-})
